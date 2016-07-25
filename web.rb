ede94daebf8e35661dea2961c80ddb3d3e28abab
Kevin Attfield <kevin@stembolt.com>
2016-07-24 17:26:43 -0700

    fixup! Add start game reponse

--- web.rb
+++ web.rb
@@ -4,14 +4,14 @@ require 'json'
 set :bind, '0.0.0.0'
 
 post '/start' do
-    responseObject = {
-        name: "Serpentor",
-        color: "#C32327",
-        head_url: "http://i.imgur.com/9Zkmwgm.png",
-        taunt: "As of now, your little project is deader than disco!"
-    }
+  responseObject = {
+    name: "Serpentor",
+    color: "#C32327",
+    head_url: "http://i.imgur.com/9Zkmwgm.png",
+    taunt: "As of now, your little project is deader than disco!"
+  }
 
-    return responseObject.to_json
+  return responseObject.to_json
 end
 
 post '/move' do
